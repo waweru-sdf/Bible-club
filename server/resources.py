6dbc120358b88d5ab9c989c6d6371d029a7ee539
waweru-sdf <felixwaweru200@gmail.com>
2025-09-22 22:27:25 +0300

    patch and delete for a user

--- server/resources.py
+++ server/resources.py
@@ -15,3 +15,23 @@ class UserListResource(Resource):
         db.session.add(user)
         db.session.commit()
         return user.to_dict(), 201
+
+class UserResource(Resource):
+    def get(self, id):
+        user = User.query.get_or_404(id)
+        return user.to_dict(), 200
+
+    def patch(self, id):
+        user = User.query.get_or_404(id)
+        data = request.get_json()
+        if "name" in data: user.name = data["name"]
+        if "email" in data: user.email = data["email"]
+        if "password" in data: user.password = data["password"]
+        db.session.commit()
+        return user.to_dict(), 200
+
+    def delete(self, id):
+        user = User.query.get_or_404(id)
+        db.session.delete(user)
+        db.session.commit()
+        return {"message": "User deleted"}, 204
